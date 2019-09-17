e29252e9a2f18876c662cbceceb07baab896feb2
Marius Gripsgard <marius@ubports.com>
2019-09-18 00:08:01 +0200

    [tool] Add help and move get under get subcommand

--- tools/device-info.cpp
+++ tools/device-info.cpp
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
- 
+
 #include "deviceinfo.h"
 #include <string>
 #include <memory>
@@ -26,61 +26,73 @@ void print(std::string str) {
     std::cout << str << std::endl;
 }
 
+int get(std::string arg, std::shared_ptr<DeviceInfo> info) {
+    if (arg == "Name") {
+        print(info->name());
+        return 0;
+    }
+    if (arg == "PrettyName") {
+        print(info->prettyName());
+        return 0;
+    }
+    if (arg == "DeviceType") {
+        print(DeviceInfo::deviceTypeToString(info->deviceType()));
+        return 0;
+    }
+    if (arg == "DriverType") {
+        print(DeviceInfo::driverTypeToString(info->driverType()));
+        return 0;
+    }
+    if (arg == "GridUnit") {
+        print(std::to_string(info->gridUnit()));
+        return 0;
+    }
+    if (arg == "SupportedOrientations") {
+        print(info->supportedOrientations());
+        return 0;
+    }
+    if (arg == "PrimaryOrientation") {
+        print(info->primaryOrientation());
+        return 0;
+    }
+    if (arg == "PortraitOrientation") {
+        print(info->portraitOrientation());
+        return 0;
+    }
+    if (arg == "InvertedPortraitOrientation") {
+        print(info->invertedPortraitOrientation());
+        return 0;
+    }
+    if (arg == "LandscapeOrientation") {
+        print(info->landscapeOrientation());
+        return 0;
+    }
+    if (info->contains(arg)) {
+        print(info->get(arg, ""));
+        return 0;
+    }
+
+    print("Unknown prop");
+    return 1;
+}
+
+void help() {
+    print("device-info usage:");
+    print("device-info get [prop]");
+}
+
 int main (int argc, char *argv[]) {
 
     std::shared_ptr<DeviceInfo> info = std::make_shared<DeviceInfo>();
 
-    std::string argv1 = argv[1];
-    if (argv1 == "name") {
-        print(info->name());
-        return 0;
-    }
-    if (argv1 == "prettyName") {
-        print(info->prettyName());
-        return 0;
-    }
-    if (argv1 == "deviceType") {
-        print(DeviceInfo::deviceTypeToString(info->deviceType()));
-        return 0;
-    }
-    if (argv1 == "driverType") {
-        print(DeviceInfo::driverTypeToString(info->driverType()));
-        return 0;
-    }
-    if (argv1 == "gridUnit") {
-        print(std::to_string(info->gridUnit()));
-        return 0;
+    if (argc == 3) {
+        std::string argv1 = argv[1];
+        if (argv1 == "get") {
+            return get(std::string(argv[2]), info);
+        }
+        // TODO: add set function
     }
 
-    if (argv1 == "supportedOrientations") {
-        print(info->supportedOrientations());
-        return 0;
-    }
-
-    if (argv1 == "primaryOrientation") {
-        print(info->primaryOrientation());
-        return 0;
-    }
-
-    if (argv1 == "portraitOrientation") {
-        print(info->portraitOrientation());
-        return 0;
-    }
-
-    if (argv1 == "invertedPortraitOrientation") {
-        print(info->invertedPortraitOrientation());
-        return 0;
-    }
-
-    if (argv1 == "landscapeOrientation") {
-        print(info->landscapeOrientation());
-        return 0;
-    }
-
-    if (info->contains(argv1)) {
-        print(info->get(argv1, ""));
-    }
-
-    print("Unknown prop");
+    help();
     return 1;
 } 
